a03dcc3a6d148ca05a99a64a2355b045179cf42f
Hussainforvercel <iqradaud643@gmail.com>
2025-03-20 01:00:18 +0800

    bundle apis added

--- Routes/contact.routes.js
+++ Routes/contact.routes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 const router = express.Router();
 import submitContactForm from '../Controllers/contact.controller.js';
-import verifyEmail from '../Controllers/verify.email.controller.js';
 
 router.post('/', submitContactForm);
 
