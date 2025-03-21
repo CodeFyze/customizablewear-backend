e369e7beb8406dbfb929e2316c3d591dae9eb939
Hussainforvercel <iqradaud643@gmail.com>
2025-03-22 00:10:00 +0800

    auth removed

--- Routes/bundleRoute.js
+++ Routes/bundleRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Define the route to handle the form submission
 router.post("/add", authenticate , addBundle);
-router.get("/", authenticate, getAllBundles);
+router.get("/", getAllBundles);
 router.put("/update", authenticate, updateBundle);
 router.delete("/delete", authenticate, deleteBundle);
 router.get("/id", authenticate, getBundleById);
